fe93e76333ed9a40d82a15878f8b6aafccbc611c
DuMengYa <3153954633@qq.com>
2020-09-19 00:46:52 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,19 @@
 $.ajaxPrefilter(function(options){
   // console.log(options.url);
   options.url = ' http://ajax.frontend.itheima.net' + options.url
-  // http://ajax.frontend.itheima.net
+  
+  if( options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization : localStorage.getItem('token')||''
+    }
+  }
+
+  options.complete = function(res){
+    if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+      
+      localStorage.removeItem('token')
+
+      location.href = '/login.html'
+    }
+  }
 })
